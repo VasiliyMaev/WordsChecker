201382e22e31be018d297ee0be118da90ce6cba2
Vasiliy Maev <sweekom@gmail.com>
2022-10-24 12:32:01 +0300

    Task 2. HashSet

---
+++ src/WordsChecker.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class WordsChecker {
+    private Set<String> text;
+
+    public WordsChecker(String text) {
+        this.text = new HashSet<>(List.of(text.split("\\P{IsAlphabetic}+")));
+    }
+
+    public boolean hashText(String word) {
+        return text.contains(word);
+    }
+}
